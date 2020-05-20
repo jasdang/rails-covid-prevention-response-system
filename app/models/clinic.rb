cfa6eb8c50961761c990eb83963babe5d773ea76
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-20 23:28:12 +0800

    typo

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,6 +1,6 @@
 class Clinic < ApplicationRecord
   validates :address, presence: true
-  validates :capacity, presence: true, numericality: { only_integer: true}
+  validates :capacity, presence: true, numericality: { only_integer: true }
   # relationship
   has_many :appointments
   has_many :citizens, through: :appointments
