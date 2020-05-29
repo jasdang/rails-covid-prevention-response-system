f8ba9f5873697fbcb81e37ae3fbe81bf55c56ca8
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-29 23:03:03 +0800

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/register', to: 'citizens#register'
+  post '/citizens', to: 'citizens#create'
+  get '/signin', to: 'citizens#signin'
+  post '/signin', to: 'citizens#authenticate'
+
+  # get '/citizens/:id/edit', to: 'citizens#edit'
+  # put '/citizens/:id', to: 'citizens#update'
+  # get '/citizens/:id/appointments/new', to: 'appointments#new'
+  # post 'citizens/:id/appointments', to: 'appointments#create'
+  resources :citizens, only: [:create, :edit, :update] do
+    resources :appointments, only: [:new, :create]
+  end
+
+  resources :clinics, except: [:destroy]
 end
